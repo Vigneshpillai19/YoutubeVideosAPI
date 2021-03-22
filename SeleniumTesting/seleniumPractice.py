1d0ebb8ee743165617b63cc6928e0938e4baa19c
Vignesh <2017.vignesh.pillai@ves.ac.in>
2021-03-22 15:07:38 +0530

    SeleniumTesting Path Error Fixed

--- SeleniumTesting/seleniumPractice.py
+++ SeleniumTesting/seleniumPractice.py
@@ -1,10 +1,15 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 import time
 
-browser = webdriver.Firefox(executable_path="../drivers/geckodriver")
+#parent_path = os.path.dirname(os.getcwd())
+#os.chdir(parent_path)
+#print(os.getcwd() + "/drivers/geckodriver")
+
+browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
 browser.get('http://localhost:3000')
 browser.find_element(By.NAME, "q").send_keys("Selenium", Keys.RETURN)
 
